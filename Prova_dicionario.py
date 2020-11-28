b8adf55770b1fbdc8ba0b3644c258c15a8bca36c
FagnerRumenigg <rumeniggmoraes@gmail.com>
2020-11-28 00:17:27 -0300

    Atualizando projeto Prova_dicionario

--- Prova_dicionario.py
+++ Prova_dicionario.py
@@ -1,28 +1,28 @@
 answer = 's'
 perguntas = {
     'Pergunta 1': {
-        'pergunta': 'Quanto é 410+10',
-        'respostas': {'a': '420', 'b': '250', 'c': '153', 'd': '419', },
+        'pergunta': 'Quanto é 410+10',#Esquerda fica a key, direita a value
+        'respostas': {'A': '420', 'B': '250', 'C': '153', 'D': '419', },
         'resposta_certa': 'a',
     },
     'Pergunta 2': {
         'pergunta': 'Complete o nome Stephen ...',
-        'respostas': {'a': 'Joe', 'b': 'Jack', 'c': 'King', 'd': 'Reaper', },
+        'respostas': {'A': 'Joe', 'B': 'Jack', 'C': 'King', 'D': 'Reaper', },
         'resposta_certa': 'c',
     },
     'Pergunta 3': {
         'pergunta': 'Quem foi "Jack, The Reaper" ',
-        'respostas': {'a': 'Um historiador', 'b': 'Um cantor', 'c': 'Um escritor', 'd': 'Um serial Killer', },
+        'respostas': {'A': 'Um historiador', 'B': 'Um cantor', 'C': 'Um escritor', 'D': 'Um serial Killer', },
         'resposta_certa': 'd',
     },
     'Pergunta 4': {
         'pergunta': 'Qual o sobrenome do casal que protagonizam os filmes "Invocação do mal" ',
-        'respostas': {'a': 'Hill', 'b': 'Bill', 'c': 'Warren', 'd': 'Bing', },
+        'respostas': {'A': 'Hill', 'B': 'Bill', 'C': 'Warren', 'D': 'Bing', },
         'resposta_certa': 'c',
     },
     'Pergunta 5': {
         'pergunta': 'A Terra é plana? ?',
-        'respostas': {'a': 'Sim', 'b': 'Não', 'c': 'Gente?', 'd': 'Não sei', },
+        'respostas': {'A': 'Sim', 'B': 'Não', 'C': 'Talvez', 'D': 'Não sei', },
         'resposta_certa': 'b',
     },
 }
@@ -31,29 +31,31 @@ print()
 while answer != 'n':
 
     respostas_certas = 0
-    for pk,pv in perguntas.items():
-        print (f'{pk}: {pv["pergunta"]}')
+    for pk, pv in perguntas.items():
+        print(f'{pk}: {pv["pergunta"]}')
 
         print('Escolha a opção correta: ')
-        for rk,rv in pv['respostas'].items():
+        for rk, rv in pv['respostas'].items():
             print(f'[{rk}]: {rv}')
 
         resposta_usuario = input('Sua resposta: ')
 
         if resposta_usuario == pv['resposta_certa']:
+            print(f'Resposta correta')
             respostas_certas += 1
         else:
             respostas_certas += 0
+            for rcv in pv['resposta_certa']:
+                print(f'A opção correta é: {rcv}')
 
         print()
 
     qtde_perguntas = len(perguntas)
-    porcentagem_acerto =  respostas_certas / qtde_perguntas * 100
+    porcentagem_acerto = respostas_certas / qtde_perguntas * 100
 
     print(f'Você acertou {respostas_certas} respostas.')
     print(f'Sua porcertagem de acerto foi de {porcentagem_acerto}%')
     print()
 
-    answer = input ('Você quer fazer de novo ? (s/n)')
+    answer = input('Você quer fazer de novo ? (s/n)')
     print()
-
